3e717928f739b7323a29ae1b8cae61c0f3bbf1a2
ZSYblah <zsyblah@gmail.com>
2022-11-08 02:35:05 +0100

    a

--- test.R
+++ test.R
@@ -1,7 +1,17 @@
-bo <- read.csv("bo.csv")
 bo_central <- read.csv("bo_central.csv")
 
-bo_mat <- as.matrix(bo)
 bo_central_mat <- as.matrix(bo_central)
 
-View(friedman.test(bo_central_mat))
+# Part II Q3: Apply friedman test to the centralized BO data
+# View(friedman.test(bo_central_mat))
+
+# Part II Q3: Apply Wilcoxon test
+ailen_hand <- bo_central["Alien.Hand"]
+matched_hand <- bo_central["Matched.Hand"]
+mismatched_hand <- bo_central["Mismatched.Hand"]
+
+# View(wilcox.test(alien_hand[, 1], matched_hand[, 1]))
+# View(res <- wilcox.test(alien_hand[, 1], mismatched_hand[, 1]))
+# View(wilcox.test(matched_hand[, 1], mismatched_hand[, 1]))
+
+# Part
--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 import csv
 import pandas as pd
-import parse
 
-data = parse.load_json("data.json")
-
+# Read to DataFrame for Part II question 2
 responses_csv = open("Responses.csv")
 responses_df = pd.DataFrame(csv.DictReader(responses_csv))
 
-bo = responses_df.query('Measure == "Body Ownership"')[["PID", "Condition", "Response"]]
+# Get Body Ownership
+bo = responses_df.query('Measure == "Body Ownership"')[["PID", "Condition", "Response"]].reindex()
 
 bo["Response"] = bo["Response"].astype(float)
 
 bo.to_csv("bo.csv")
 
-bo_central = bo.groupby(["PID", "Condition"]).agg(lambda x: x.value_counts().index[0]).reset_index()
+# Apply central tendency to Body Ownership
+# And reshape the data, the row names will be PID, the column names will be Condition
+
+# Mean
+# bo_central = bo.groupby(["PID", "Condition"]).mean("Response").unstack()
+
+# Mode
+bo_central = bo.groupby(["PID", "Condition"]).agg(lambda x: x.value_counts().index[0])
+
+bo_central = bo_central.reset_index().pivot(index="PID", columns="Condition", values="Response")
+
 bo_central.to_csv("bo_central.csv")
